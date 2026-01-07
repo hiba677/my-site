4ce16195288501602f7a082ffd6c43d24b394a86
hiba <hibqamsaq@gmail.com>
2026-01-07 20:40:21 +0100

    last update for about us

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Hero Slider full-screen
-setInterval(() => {
-    currentHero = (currentHero + 1) % heroImages.length;
-    heroImgElement.src = heroImages[currentHero];
-}, 3000);
-
-
-
-// Boutons "Voir tout"
-document.querySelectorAll('.section-header button').forEach(btn => {
-    btn.addEventListener('click', () => {
-        alert("Vous avez cliqué sur 'Voir tout'");
-    });
-});
 
 // Bouton Subscribe
 document.querySelector('.subscription-form button').addEventListener('click', () => {
@@ -30,30 +16,6 @@ document.querySelectorAll('.product button').forEach(btn => {
 });
 
 
-// Boutons "Voir les détails"
-document.querySelectorAll('.product button').forEach(btn => {
-    btn.addEventListener('click', () => {
-        const productName = btn.parentElement.querySelector('img').alt;
-        alert(`Vous avez cliqué pour voir plus de détails sur : ${productName}`);
-    });
-});
-// Exemple: filtre catégorie
-        // Exemple: filtre catégorie
-        const filterButtons = document.querySelectorAll('.filters button');
-        const products = document.querySelectorAll('.product-card');
-
-    filterButtons.forEach(btn => {
-     btn.addEventListener('click', () => {
-     const category = btn.textContent.toLowerCase();
-    products.forEach(p => {
-   if (p.querySelector('h4').textContent.toLowerCase().includes(category) || category === 'tous') {
-    p.style.display = 'block';
-    } else {
-     p.style.display = 'none';
-   }
-   });
-   });
-  });
 // ===== MODAL PRODUIT =====
 const modal = document.getElementById("productModal");
 const modalImg = document.getElementById("modalImage");
